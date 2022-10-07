a8e75fb8350153ceab6ad55ab434d213f3fbcae6
ilahe salehova <ferhad.musayev.00@gmail.com>
2022-10-07 12:40:32 +0400

    one to many poli

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
 
+
 class Image extends Model
 {
     use HasFactory;
